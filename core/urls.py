47b1e4f185f997e6fbec8fda1dfe57d3be29686e
Francisco Michea <franciscomichea@MacBook-Air-de-Francisco.local>
2026-02-08 19:15:13 -0300

    Fix: Refactor BI Report to ViewSet and fix Pandas types

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,25 @@
+from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect 
+from django.shortcuts import redirect
 from rest_framework.routers import DefaultRouter
 from inventario.views import (
     ProductViewSet, 
     CategoriaViewSet, 
     DashboardStatsView, 
-    ReporteAnaliticoView
+    ReporteViewSet 
 )
+
 def root_redirect(request):
-    return redirect('/api/') 
+    return redirect('/api/')
 
 router = DefaultRouter()
 router.register(r'productos', ProductViewSet)
 router.register(r'categorias', CategoriaViewSet)
+router.register(r'reporte-bi', ReporteViewSet, basename='reporte-bi')
 
 urlpatterns = [
     path('', root_redirect, name='root'),
+    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/dashboard-stats/', DashboardStatsView.as_view(), name='dashboard-stats'),
-    path('api/reporte-bi/', ReporteAnaliticoView.as_view(), name='reporte_bi') 
 ]
